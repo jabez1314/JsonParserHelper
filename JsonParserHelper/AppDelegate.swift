237ea25b03683e33aedcdc6d4ffea193d88566aa
jabez <liyan@quaner.com.cn>
2015-05-04 10:25:26 +0800

    Initial Commit

---
+++ JsonParserHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JsonParserHelper
+//
+//  Created by Jabez on 15/5/4.
+//  Copyright (c) 2015年 John. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JsonParserHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JsonParserHelper
+//
+//  Created by Jabez on 15/5/4.
+//  Copyright (c) 2015年 John. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
